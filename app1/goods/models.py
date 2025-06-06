f8c1c883d32c56d1352e06cf93555630c16b5684
ReZoR-Q2 <rezorgerman@gmail.com>
2025-06-06 22:28:18 +0300

    bata base

--- app1/goods/models.py
+++ app1/goods/models.py
@@ -14,6 +14,9 @@ class Categories(models.Model):
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
         
+    def __str__(self):
+        return self.name
+        
         
         
 class Products(models.Model):
@@ -30,3 +33,6 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantily}'
